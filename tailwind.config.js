836c637cf70d4ad7ee102fc3a5f7d87b626b7e0e
A. Garner Designs <101416832+atgarner23@users.noreply.github.com>
2023-01-31 13:42:47 -0800

    create tailwind css integration

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      'white': '#E6F4F7',
+      'black': '#17151A',
+      'orange': '#EBA730',
+      'teal': '#33BA9D',
+      'blue': {
+        light: '#647A82',
+        DEFAULT: '#073642',
+        dark: '#002B36',
+      },
+    },
+    extend: {
+      fontFamily: {
+        serif: ['"Alfa Slab One"', ...defaultTheme.fontFamily.serif],
+        sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
