b0b04123c5a81ec1622b2d64cfefcf2797323861
A. Garner Designs <101416832+atgarner23@users.noreply.github.com>
2023-02-13 20:01:22 -0800

    create mobile menu

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -1,1 +1,14 @@
-const mobileBtn = document.querySelector('#mobileToggle');
+const openBtn = document.querySelector('#mobileOpen');
+const closeBtn = document.querySelector('#mobileClose');
+const modal = document.querySelector('#overlay');
+const mobileNav = document.querySelector('#mobileNav');
+
+openBtn.addEventListener('click', () => {
+    modal.classList.toggle('hidden');
+});
+closeBtn.addEventListener('click', () => {
+    modal.classList.toggle('hidden');
+});
+mobileNav.addEventListener('click', () => {
+    modal.classList.toggle('hidden');
+});
