6a91e00572109df5c3c48a5c18c79c5f4b29ae57
A. Garner Designs <101416832+atgarner23@users.noreply.github.com>
2023-02-21 16:06:45 -0800

    Add Favicon.ico

--- index.php
+++ index.php
@@ -12,6 +12,10 @@ require_once('includes/functions.php');
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Andy Garner Web Portfolio</title>
   <link href="/dist/output.css" rel="stylesheet">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Open+Sans:wght@300;400;600;700&display=swap"
